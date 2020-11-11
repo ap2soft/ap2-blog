7bb964740a324c774c446b3f32ebde02a359e86f
Andrew P <andreich1980@gmail.com>
2020-11-12 01:03:27 +0300

    Config: base url

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Str;
 
 return [
-    'baseUrl' => '',
+    'baseUrl' => 'http://localhost:3000/',
     'production' => false,
     'siteName' => 'AP2 | Full stack web developer',
     'siteDescription' => 'Full Stack Web Developer. Personal Website.',
