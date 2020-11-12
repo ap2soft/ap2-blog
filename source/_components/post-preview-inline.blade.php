11858bcd772ff02d5832ea686b1b24f29e2e301a
Andrew P <andreich1980@gmail.com>
2020-11-12 16:43:24 +0300

    Blog main page

--- source/_components/post-preview-inline.blade.php
+++ source/_components/post-preview-inline.blade.php
@@ -1,21 +1,30 @@
-<div class="flex flex-col mb-4">
-    <p class="text-gray-700 font-medium my-2">
-        {{ $post->getDate()->format('F j, Y') }}
-    </p>
+<div class="flex flex-col">
+    <div class="relative" style="padding-bottom: 33%">
+        @if ($post->cover_image)
+            <img src="{{ $post->cover_image }}" alt="{{ $post->title }}" class="absolute w-full h-full object-cover">
+        @else
+            <div class="absolute w-full h-full bg-gradient-to-r from-red-600 to-gray-800 | flex justify-center items-center">
+               <a href="{{ $post->getUrl() }}" title="Read more - {{ $post->title }}" class="text-gray-300 text-xl font-serif font-bold tracking-wide hover:underline">
+                    {{ $post->title }}
+                </a>
+            </div>
+        @endif
+    </div>
 
-    <h2 class="text-3xl mt-0">
-        <a
-            href="{{ $post->getUrl() }}"
-            title="Read more - {{ $post->title }}"
-            class="text-gray-900 font-extrabold"
-        >{{ $post->title }}</a>
+    <h2 class="mt-6 text-3xl">
+        <a href="{{ $post->getUrl() }}" title="Read more - {{ $post->title }}" class="text-gray-300 font-serif font-bold tracking-wide hover:underline">
+            {{ $post->title }}
+        </a>
     </h2>
 
-    <p class="mb-4 mt-0">{!! $post->getExcerpt(200) !!}</p>
+    <p class="mt-4 text-gray-500">
+        {!! $post->getExcerpt(200) !!}
+    </p>
 
-    <a
-        href="{{ $post->getUrl() }}"
-        title="Read more - {{ $post->title }}"
-        class="uppercase font-semibold tracking-wide mb-2"
-    >Read</a>
+    <div class="mt-4 text-gray-600 text-sm font-medium flex items-center space-x-1">
+        <div class="w-4 h-4">@include('_svg.calendar')</div>
+        <div>
+            {{ $post->getDate()->format('F j, Y') }}
+        </div>
+    </div>
 </div>
